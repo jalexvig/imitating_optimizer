a9bbd50582f924a35b885d7b640beb7f8fa13bde
Alex Vig <jalexvig@gmail.com>
2018-05-14 16:00:18 -0400

    Added weight regularization after meta-optimizer

--- notes.md
+++ notes.md
@@ -3,3 +3,7 @@
 LSTM hidden state can explode (or potentially vanish?) since we necessarily only train for a certain number of timesteps, but test on an independent/arbitrary number. This can cause parameters of system to drift despite RNN output being close to theoretical parameter update (via optimizer).
 
 In other words state doesn't have the opportunity to hurt performance very much during training, but it does during evaluation.
+
+### Weight regularization
+
+This helps stability. Seems to be more effective when added to meta_optimizer rather than predicted by meta-optimizer (see performance of branch `add_weight_reg_metamodel`).
--- src/test.py
+++ src/test.py
@@ -8,6 +8,8 @@ import numpy as np
 from src import CONFIG
 from src.train import MetaOptimizer
 
+WEIGHT_DECAY = 1e-3
+
 
 def test():
 
@@ -25,19 +27,25 @@ def test():
 
         deltas_pred, state = meta_learner(grads, state)
 
+        params = torch.cat([p.reshape(-1) for p in model.params])
+
         l = (deltas_opt - deltas_pred).norm()
 
         perc_diff = (deltas_opt - deltas_pred) / (deltas_opt + 1e-8)
 
-        if torch.isnan(l).any() or i > 8000:
+        if torch.isnan(l).any():
+            params_stats = describe(params.abs().data.numpy(), axis=None)
+            print(i, params_stats)
             import ipdb; ipdb.set_trace()
 
         if i % 100 == 0:
             stats = describe(perc_diff.abs().data.numpy(), axis=None)
+            params_stats = describe(params.abs().data.numpy(), axis=None)
             print(i,
                   l.item(),
                   losses[0].item(),
                   (stats.minmax, stats.mean, stats.variance),
+                  (params_stats.minmax, params_stats.mean, params_stats.variance),
                   )
 
         if update_params:
@@ -48,5 +56,6 @@ def test():
             size = np.prod(param.shape)
             # deltas_pred = grads.view(-1) * -0.01
             delta = deltas_pred[j: j + size].reshape(param.shape)
+            delta -= WEIGHT_DECAY * params[j: j + size].reshape(param.shape)
             param.data.add_(delta)
             j += size
